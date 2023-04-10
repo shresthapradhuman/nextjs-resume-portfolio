134cb73ae2ebc6c72a044929b8bebf3a93b326bd
shrestha@veriteworks.co.jp <shrestha@veriteworks.co.jp>
2023-04-10 11:56:30 +0900

    added linkden share button

--- pages/forums/[slug].js
+++ pages/forums/[slug].js
@@ -3,7 +3,12 @@ import path from "path";
 import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { FacebookIcon, FacebookShareButton } from "next-share";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+} from "next-share";
 import { useRouter } from "next/router";
 function Post({
   frontmatter: { title, date, cover_image, category },
@@ -12,15 +17,12 @@ function Post({
 }) {
   let route = useRouter();
   return (
-    <section className="forum my-5 mt-3 shadow shadow-slate-400 rounded-2xl py-5 px-5 bg-white">
-      <h2 className="text-3xl font-medium mb-1">{title}</h2>
-      <div className="flex justify-between flex-wrap">
+    <section className="px-5 py-5 my-5 mt-3 bg-white shadow forum shadow-slate-400 rounded-2xl">
+      <h2 className="mb-1 text-3xl font-medium">{title}</h2>
+      <div className="flex flex-wrap justify-between">
         <p>
           {category.map((item, key) => (
-            <span
-              key={key}
-              className="px-2 py-1 bg-orange-400 mr-1 rounded-md"
-            >
+            <span key={key} className="px-2 py-1 mr-1 bg-orange-400 rounded-md">
               {item}
             </span>
           ))}
@@ -29,14 +31,18 @@ function Post({
       </div>
       <hr></hr>
       <ReactMarkdown>{content}</ReactMarkdown>
-      <div className="flex items-center border-t-2 mt-5">
+      <div className="flex items-center mt-5 border-t-2">
         <p className="mr-2 text-xl font-medium">Share On :</p>
         <FacebookShareButton
           url={`https://shresthapradhuman.com.np/${route.asPath}`}
           quote={title}
+          hashtag={title}
         >
           <FacebookIcon size={29} />
         </FacebookShareButton>
+        <LinkedinShareButton url={`https://shresthapradhuman.com.np/${route.asPath}`}>
+          <LinkedinIcon size={29} />
+        </LinkedinShareButton>
       </div>
     </section>
   );
